b1f974b237092931de664ba9427d8ab36ae19072
Ram-Devops-Py <55368979+Ram-Devops-Py@users.noreply.github.com>
2019-09-19 14:13:34 +0100

    Add files via upload
    Day1_Adv_Numpy

---
+++ Day01_Advanced_Python.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[20]:
+
+
+# A 1d array from a list.
+import numpy as np
+list1 = [1,2,3]
+array1 = np.array(list1)
+
+# Print the array and it's type
+print(array1)
+print(type(array1))
+
+
+# In[21]:
+
+
+array1 + 3
+
+
+# In[32]:
+
+
+# 2d arry from 
+list2 = [[1,2,3],[4,5,6],[7,8,9]]
+array2 = np.array(list2)
+print(array2)
+
+
+# In[ ]:
+
+
+
+
